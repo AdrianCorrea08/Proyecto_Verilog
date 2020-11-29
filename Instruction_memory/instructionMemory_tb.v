c2b9732871e94a4bb770259716c3c25da0dbb869
andresxz32 <andres.ospina12@utp.edu.co>
2020-11-29 11:45:02 -0500

    se crean ALU y register memory sin sus tb

--- Instruction_memory/instructionMemory_tb.v
+++ Instruction_memory/instructionMemory_tb.v
@@ -4,19 +4,15 @@
 module InstructionMemory_tb;
 
 reg clk;
-reg  [7:0] address0;
-reg  [7:1] address1;
-reg  [7:2] address2;
-reg  [7:3] address3;
-reg  [7:4] address4;
-reg  [7:5] address5;
-reg  [7:6] address6;
-reg  [7:7] address7;
+reg  [3:0] address0;
+reg  [3:1] address1;
+reg  [3:2] address2;
+reg  [3:3] address3;
 wire [31:0]	 instruction;
 
 InstructionMemory im(clk, address, instruction);
 
-assign instruction = {address0,address1,address2,address3,address4,address5,address6,address7};
+assign instruction = {address0,address1,address2,address3};
 
 initial begin
 
