cfe913b869b1c044d3b13e4168a5de6e78790789
AdrianCorrea08 <adrian.correa@utp.edu.co>
2020-12-04 01:26:36 -0500

    agregando Register Memory

--- registerMemory/registerMemory.v
+++ registerMemory/registerMemory.v
@@ -1,52 +1,39 @@
-module registerMemory(clk,rs1 ,rs2, writeRegister,writeData,control,data1,data2);
+`timescale 1ns / 1ps
 
-input clk;
-input [19:15] readRegister1;
-input [24:20] readRegister2;
-input [11:7] writeRegister;
-//input [] writeData -->No se sabe, viene del multiplexor
-//preguntar la data1 y data2 de cuantos bits salen
+module RegisterMemory(rs1,rs2,rd,WriteData,RegWrite,Data1,Data2,clk);
 
+input    clk;      //La clk para activar la escritura
+input    RegWrite; //el control de escritura
 
-endmodule
+// los números de registro para leer o escribir
+input  [5:0] rs1;        
+input  [5:0] rs2;        
+input  [5:0] rd; 
 
-module reg_file(IN, OUT1, OUT2, INADDRESS, OUT1ADDRESS, OUT2ADDRESS, WRITE, CLK, RESET) ;
- //Initalizing inputs
- input [2:0] INADDRESS;
- input [2:0] OUT1ADDRESS;
- input [2:0] OUT2ADDRESS;
- input WRITE;
- input CLK;
- input RESET;
- input [7:0] IN;
- //initializing outputs
- output [7:0] OUT1;
- output [7:0] OUT2;
- //initializing register variables
-integer i; 
- //creating the register array
- reg [7:0] regFile [0:7]; 
- //resetting the registers if the reset is 1 as a level triggered input
- always@(*)
- if (RESET == 1) begin 
- #2
- for (i = 0; i < 8; i = i + 1) 
- begin
- regFile [i] = 8'b00000000 ; 
- end 
- 
- end 
- //this always block runs of the positive edge of the clock and write to the register if write is ennable
- always@(posedge CLK)
- begin 
- if(WRITE == 1'b1 && RESET == 1'b0) begin
- #2 regFile [INADDRESS] = IN; //this includes the write reg delay
- //$monitor($time,” %b”,regFile [INADDRESS]);
- end 
- end
- 
- //this is for reading the inputs from the registers
- //this part was modified after the lab 5 part 3 submission
- assign #2 OUT1 = regFile[OUT1ADDRESS];
- assign #2 OUT2 = regFile[OUT2ADDRESS];
+//datos para escribir
+input  [63:0] WriteData; 
+
+//los valores de registro leídos
+output  [63:0] Data1;    
+output  [63:0] Data2;    
+
+//32 registros cada uno de 32 bits de longitud
+reg [63:0] RF [31:0];
+
+assign Data1 = RF[rs1];
+assign Data2 = RF[rs2];
+//integer i;
+
+/*initial begin
+ for(i=0;i<8;i=i+1)
+   RF[i] <= 64'd0;
+ end*/
+
+ //escribir el registro con un nuevo valor si RegWrite es alto
+always @ (posedge clk ) begin
+  if(RegWrite) begin
+    RF[rd] <= WriteData;
+  end
+end
+
 endmodule
